cc8c4772834b3a5d3aba47dc0466ea707897a143
Vadym Verba <71729845+vhyver@users.noreply.github.com>
2022-09-26 20:14:08 +0300

    Finished

---
+++ 2D Lab2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed = 10f;
+    public float jumpHeight = 15f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private CapsuleCollider2D capsuleCollider;
+    Vector2 movementVector;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    void Update()
+    {
+        Vector2 playerVelocity = new Vector2(movementVector.x * movementSpeed, rb.velocity.y);
+        rb.velocity = playerVelocity;
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        anim.SetBool("isJump", false);
+    }
+
+
+    private void OnMove(InputValue value)
+    {
+        movementVector = value.Get<Vector2>();
+        if (!anim.GetBool("isJump"))
+            anim.SetBool("isRun", true);
+    }
+    private void OnJump(InputValue value)
+    {
+        if (!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) return;
+        if (value.isPressed)
+        {
+            if (!anim.GetBool("isRun"))
+                anim.SetBool("isJump", true);
+            rb.velocity += new Vector2(0f, jumpHeight);
+        }
+    }
+}
